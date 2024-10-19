77a379cd8ee2d22c6ee4753f3986bb198fa98813
Xinchen Liu <mike000708@me.com>
2024-10-19 16:39:14 -0700

    add deploy scripts

---
+++ packages/contracts/script/DeployQiaoFactory.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "forge-std/Script.sol";
+import "../src/QiaoFactory.sol";
+
+contract DeployQiaoFactory is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        QiaoFactory factory = new QiaoFactory();
+
+        vm.stopBroadcast();
+
+        console.log("Factory deployed at:", address(factory));
+    }
+}
